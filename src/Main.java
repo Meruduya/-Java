e02c06996aa3a2c84d0ee3f73f19b4a4f93b8907
Veronika <korynaitroyd@gmail.com>
2025-05-14 18:49:20 +0300

    Written by Mine

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int eaters = 5; // сколько людей будут есть
+
+        int water = 3000; // миллилитров воды
+        int patatoes = 5; // картофелин
+        int chicken = 6; // куринных бедер
+        int spices = 10; // ложек специй
+
+        System.out.println("Сварил суп. На одгого человека вышло:");
+        System.out.println((water / eaters) + " миллилитров(а) воды");
+        System.out.println((patatoes / eaters) + " картофелин(ф/ы)");
+        System.out.println((patatoes / eaters) + " куринных(ое) бёдер(ро)");
+        System.out.println((spices / eaters) + "ложек(ки/ка) специй");
     }
 }
